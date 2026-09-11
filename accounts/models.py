bdd8842c757a8f0bb9063bfa7ceb837f317b5d5c
lucas5g <lucasdesousa19@hotmail.com>
2026-09-11 19:39:24 -0300

    Add initial Django project structure with custom user model and Docker setup

---
+++ accounts/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    # Add any additional fields or methods you want for your custom user model
+    pass
