6963f57bdf4b16aa2c2c1490355e7817b7d9c8c7
lucas5g <lucasdesousa19@hotmail.com>
2026-09-12 18:02:06 -0300

    Add signup page tests, fix signup view class name, and update form method in signup template

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from .forms import CustomUserCreationForm
 
 
 # Create your views here.
-class SignupPageview(generic.CreateView):
+class SignupPageView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
--- django_project/settings.py
+++ django_project/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "db",
+        "HOST": "localhost",
         "PORT": 5432,
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
