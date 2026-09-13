5fd70ec93db7bf74e3f5273b94c6221687f9dedd
lucas5g <lucasdesousa19@hotmail.com>
2026-09-13 18:19:46 -0300

    130

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third-party apps
+    'crispy_forms',
+    'crispy_bootstrap5',
     #Local apps
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
@@ -141,3 +144,6 @@ MAILERS = {
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
