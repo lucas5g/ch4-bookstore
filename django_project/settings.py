2675ac73083b012cb438eda3b2a39b28eebe1e4d
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2026-09-17 18:59:18 -0300

    178

--- django_project/settings.py
+++ django_project/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     #Local apps
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
+    "books.apps.BooksConfig",
 ]
 
 SITE_ID = 1
--- django_project/urls.py
+++ django_project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     # path('accounts/', include('django.contrib.auth.urls')),
     # path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
+    path('books/', include('books.urls')),
 ]
