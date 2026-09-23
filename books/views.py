ab21763be89a63e90b7140dc4762209da1faf0fc
lucas5g <lucasdesousa19@hotmail.com>
2026-09-22 22:51:06 -0300

    Require authentication for book views

--- books/views.py
+++ books/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Book
 # Create your views here.
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'books/book_list.html'
     context_object_name = 'book_list'
+    login_url = 'account_login'
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
+    login_url = 'account_login'
